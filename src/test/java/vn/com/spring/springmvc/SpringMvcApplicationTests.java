a18ba12643dd69c3d7b31cf3d8c11a3c1e9c7187
Man Nguyen Ly Minh <nguyenlyminhman@gmail.com>
2020-02-21 10:47:00 +0700

    Create Project

---
+++ src/test/java/vn/com/spring/springmvc/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.spring.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
